76b20df25c888f8f770da31914844d803a80ad6e
Inmo Jang <inmo3592@gmail.com>
2025-11-06 00:16:19 +0900

    Refactored heavily

--- main.py
+++ main.py
@@ -17,39 +17,24 @@ from modules.utils import config
 
 
 # Dynamically import the environment module and Env class
-env_module = importlib.import_module(config.get('scenario').get('environment') + ".env")
+env_module = importlib.import_module(config.get('scenario') + ".env")
 Env = getattr(env_module, "Env")
 # Initialize Env instance
-env = Env(config)
-
-async def game_loop():
-    while env.running:
-        env.handle_keyboard_events()
-
-        if not env.game_paused:
-            await env.step()
-            # Record data if time recording mode is enabled
+bt_runner = Env(config)
 
 
-    env.close()
+async def loop():
+    while bt_runner.running:
+        bt_runner.handle_keyboard_events()
+        if not bt_runner.paused:
+            await bt_runner.step()
+
+    bt_runner.close()
 
 
-
-def main():
-    bt_viz_cfg = config['simulation'].get('bt_visualiser', {})
-    if bt_viz_cfg.get('enabled', False):
-        from modules.bt_visualiser import visualise_bt
-        agent = env.agent
-        Thread(
-            target=visualise_bt, 
-            args=(0, agent.tree), 
-            daemon=True
-        ).start()
-
-    asyncio.run(game_loop())
 
 if __name__ == "__main__":
-    if config['simulation']['profiling_mode']:
+    if config['bt_runner']['profiling_mode']:
         cProfile.run('main()', sort='cumulative')
     else:
-        main()
+        asyncio.run(loop())
--- modules/base_env.py
+++ modules/base_env.py
@@ -5,27 +5,37 @@ import pygame
 class BaseEnv:
     def __init__(self, config):
         self.config = config
-        self.bt_tick_rate = config['simulation']['bt_tick_rate']
-        self.screen_height = config['simulation']['screen_height']
-        self.screen_width = config['simulation']['screen_width']        
+        self.bt_viz_cfg = config['bt_runner'].get('bt_visualiser', {})
+        self.bt_tick_rate = config['bt_runner']['bt_tick_rate']
         pygame.init()
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height), pygame.RESIZABLE)
-        self.background_color = (224, 224, 224)
+        if self.bt_viz_cfg.get('enabled', False):
+            os.environ['SDL_VIDEO_WINDOW_POS'] = "0,30"  # top-left corner
+            self.screen_height = self.bt_viz_cfg.get('screen_height',500)
+            self.screen_width = self.bt_viz_cfg.get('screen_width',500) 
+            self.screen = pygame.display.set_mode((self.screen_width, self.screen_height), pygame.RESIZABLE)
+            self.background_color = (224, 224, 224)
+            from .bt_visualiser import BTViewer
+            self.bt_visualiser = BTViewer(
+                direction=self.bt_viz_cfg.get('direction', 'Vertical')
+            )
         self.clock = pygame.time.Clock()
+        
 
     def reset(self):
         # Initialization        
         self.running = True
-        self.game_paused = False   
-
+        self.paused = False   
         self.agent = None
 
 
 
 
     async def step(self):
-        # Main simulation loop logic
+        # Main bt_runner loop logic
         await self.agent.run_tree()
+        if self.bt_viz_cfg.get('enabled', False):
+            self.bt_visualiser.render_tree(self.screen, self.agent.tree)
+            pygame.display.flip()
         self.clock.tick(self.bt_tick_rate)
 
 
@@ -41,4 +51,4 @@ class BaseEnv:
                 if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
                     self.running = False
                 elif event.key == pygame.K_p:
-                    self.game_paused = not self.game_paused
+                    self.paused = not self.paused
